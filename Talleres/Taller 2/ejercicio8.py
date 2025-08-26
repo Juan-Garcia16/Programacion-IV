40746ee4e1cdcf4dbdc79fe78a2993125e22dea9
Juan Garcia <juan.garcia16@utp.edu.co>
2025-08-25 20:29:37 -0500

    Taller 2 entregado

--- Talleres/Taller 2/ejercicio8.py
+++ Talleres/Taller 2/ejercicio8.py
@@ -24,15 +24,26 @@ class Evento:
         
     def eventos_proxima_semana(archivo = "agenda.txt"):
         archivo = open(archivo, "r")
+        print("Eventos próxima semana:")
+        for linea in archivo:
+            datos = linea.strip().split(",")
+            titulo = datos[0]
+            fecha = datos[1]
+            lista_fecha = fecha.split("-")
+            #semana siguiete actual
+            if (lista_fecha[1] == "09") and (1 <= int(lista_fecha[2]) <=7 ):
+                print(f"{titulo}: {fecha}")
+
         
 
 eventos = [
-    Evento("Concierto de Rock", "2025-08-15", "20:00", "Estadio Nacional", "Carlos Pérez"),
-    Evento("Feria del Libro", "2025-08-05", "10:00", "Centro de Convenciones", "Ana Torres"),
+    Evento("Concierto de Rock", "2025-09-07", "20:00", "Estadio Nacional", "Carlos Pérez"),
+    Evento("Feria del Libro", "2025-09-05", "10:00", "Centro de Convenciones", "Ana Torres"),
     Evento("Maratón Ciudad", "2025-11-01", "06:30", "Parque Principal", "Luis Gómez"),
     Evento("Congreso de Tecnología", "2025-08-30", "09:00", "Hotel Intercontinental", "María Rodríguez"),
-    Evento("Obra de Teatro: Hamlet", "2025-09-25", "19:30", "Teatro Central", "Pedro Ramírez"),
-    Evento("Taller de Fotografía", "2025-08-12", "15:00", "Casa de la Cultura", "Laura Sánchez")
+    Evento("Obra de Teatro: Hamlet", "2025-09-03", "19:30", "Teatro Central", "Pedro Ramírez"),
+    Evento("Taller de Fotografía", "2025-09-12", "15:00", "Casa de la Cultura", "Laura Sánchez")
 ]
         
 Evento.registrar_eventos(eventos)
+Evento.eventos_proxima_semana()
